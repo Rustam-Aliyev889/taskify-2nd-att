6055a5b8f59a2d35e6f6063b42e354cbcdbb55e4
Rustam Aliyev <rustam.aliyev889@gmail.com>
2023-06-05 21:59:20 +0100

    fixing update function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2.extras
 
 app = Flask(__name__)
 
@@ -16,11 +17,9 @@ def db_conn():
     return conn
 
 
-
 @app.route("/")
 def home():
     return render_template("base.html")
-
 
 
 @app.route("/meetings")
@@ -58,28 +57,58 @@ def new_meeting():
     conn.close()
     return redirect(url_for('meetings'))
 
-@app.route("/update", methods=['POST','GET'])
-def update():#id,topic,department,comment):
+@app.route('/edit/<id>', methods = ['POST', 'GET'])
+def get_meetings(id):
     conn = db_conn()
-    cur = conn.cursor()
-    topic = request.form["topic"]
-    department = request.form["department"]
-    comment = request.form["comment"]
-    cur.execute('''UPDATE meetings SET topic=%s, department=%s, comment=%s WHERE id=%s''', (id,)) #topic, department, comment,))
-    cur.execute()
-    conn.commit()
-    return redirect(url_for('new_meeting'))
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   
+    cur.execute('SELECT * FROM meetings WHERE id = %s', (id))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit.html', meetings = data[0])
+ 
+@app.route('/update/<id>', methods=['POST'])
+def update_meetings(id):
+    if request.method == 'POST':
+        topic = request.form["topic"]
+        date = request.form["date"]
+        time = request.form["time"]
+        department = request.form["department"]
+        comments = request.form["comments"]
+        conn = db_conn()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("""
+            UPDATE meetings
+            SET topic = %s,
+                date = %s,
+                time = %s,
+                department = %s,
+                comments = %s
+            WHERE id = %s
+        """, (topic, date, time, department, comments, id))
+        conn.commit()
+        return redirect(url_for('meetings'))
 
-#@app.route('/delete', methods=['POST', 'GET'])
-#def delete():
- #   conn = db_conn()
- #   cur = conn.cursor()
-  #  id = request.form['id']
-  #  cur.execute('''DELETE FROM meetings WHERE id = %s''',(id))
-  #  conn.commit()
-  #  cur.close()
-  #  conn.close()
-   # return redirect(url_for('meetings'))
+
+
+
+
+
+
+#@app.route("/update", methods=['POST','GET'])
+#def update():#id,topic,department,comment):
+    #conn = db_conn()
+    #cur = conn.cursor()
+    #topic = request.form["topic"]
+    #department = request.form["department"]
+    #comment = request.form["comment"]
+    #cur.execute('''UPDATE meetings SET topic=%s, date=%s, time=%s department=%s, comment=%s WHERE id=%s''', (id,)) #topic, department, comment,))
+    #cur.execute()
+    #conn.commit()
+    #return redirect(url_for('new_meeting'))
+
+
 
 @app.route('/delete <id>') #, methods=['POST', 'GET'])
 def delete(id):
